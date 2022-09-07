6a8dfb8eb990679026efb744efc7b7ce244165e5
Rustam <gadelshin2011@gmail.com>
2022-09-07 23:40:34 +0500

    создал 2 новых класса - Заполняю класс AppModel функциями игры

---
+++ app/src/main/java/com/example/tetrisnoactivity/models/AppModel.kt
@@ -0,0 +1,166 @@
+package com.example.tetrisnoactivity.models
+
+import android.graphics.Point
+import com.example.tetrisnoactivity.constants.FieldConstants
+import com.example.tetrisnoactivity.constants.CellConstants
+import com.example.tetrisnoactivity.helper.array2dofByte
+import com.example.tetrisnoactivity.SharedPreferences.AppPreferences
+import java.text.FieldPosition
+
+class AppModel {
+    var score: Int = 0
+    private var preferences: AppPreferences? = null
+
+    var currentBlock: Block? = null
+    var currentState: String = Statuses.AWAITING_START.name
+
+    private var field: Array<ByteArray> = array2dofByte(
+        FieldConstants.ROW_COUNT.value,
+        FieldConstants.COLUMN_COUNT.value
+    )
+
+    fun setPreferences(preferences: AppPreferences?) {
+        this.preferences = preferences
+    }
+
+    fun getCellStatus(row: Int, column: Int): Byte? {
+        return field[row][column]
+    }
+
+    private fun setCellStatus(row: Int, column: Int, status: Byte?) {
+        if (status != null) {
+            field[row][column] = status
+        }
+    }
+
+    fun isGameOver(): Boolean {
+        return currentState == Statuses.OVER.name
+    }
+
+    fun isGameActive(): Boolean {
+        return currentState == Statuses.ACTIVE.name
+    }
+
+    fun isGameAwaitingStart(): Boolean {
+        return currentState == Statuses.AWAITING_START.name
+    }
+
+    private fun boostScore() {
+        score += 10
+        if (score > preferences?.getHighScore() as Int) {
+            preferences?.saveHighScore(score)
+        }
+    }
+
+    private fun generateNextBlock() {
+        currentBlock = Block.createBlock()
+    }
+
+    private fun validTranslation(position: Point, shape: Array<ByteArray>): Boolean {
+        return if (position.y < 0 || position.x < 0) {
+            false
+        } else if (position.y + shape.size > FieldConstants.ROW_COUNT.value) {
+            false
+        } else if (position.x + shape[0].size > FieldConstants.COLUMN_COUNT.value) {
+            false
+        } else {
+            for (i in shape.indices) {
+                for (j in 0 until shape[i].size) {
+                    val y = position.y + i
+                    val x = position.x + j
+                    if (CellConstants.EMPTY.value != shape[i][j] && CellConstants.EMPTY.value != field[y][x]) {
+                        return false
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    private fun moveValid(position: Point, frameNumber: Int?): Boolean {
+        val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber as Int)
+        return validTranslation(position, shape as Array<ByteArray>)
+    }
+
+    fun generateField(action: String) {
+        if (isGameActive()) {
+            resetField()
+            var frameNumber: Int? = currentBlock?.frameNumber
+            var coordinate: Point? = Point()
+            coordinate?.x = currentBlock?.position?.x
+            coordinate?.y = currentBlock?.position?.y
+
+            when (action) {
+                Motions.LEFT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.minus(1)
+                }
+                Motions.RIGHT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.plus(1)
+                }
+                Motions.DOWN.name -> {
+                    coordinate?.y = currentBlock?.position?.y?.plus(1)
+                }
+                Motions.ROTATE.name -> {
+                    frameNumber = frameNumber?.plus(1)
+
+                    if (frameNumber != null) {
+                        if (frameNumber >= currentBlock?.frameCount as Int) {
+                            frameNumber = 0
+                        }
+                    }
+                }
+
+            }
+
+            if (!moveValid(coordinate as Point, frameNumber)) {
+                translateBlock(currentBlock?.position as Point, currentBlock?.frameNumber as Int)
+                if (Motions.DOWN.name == action) {
+                    boostScore()
+                    persistCellData()
+                    assessField()
+                    generateNextBlock()
+                    if (!blockAdditionPossible()) {
+                        currentState = Statuses.OVER.name;
+                        currentBlock = null;
+                        resetField(false);
+                    }
+                }
+            } else {
+                if (frameNumber != null) {
+                    translateBlock(coordinate, frameNumber)
+                    currentBlock?.setState(frameNumber, coordinate)
+                }
+            }
+        }
+    }
+
+
+    private fun blockAdditionPossible(): Any {
+
+    }
+
+    private fun assessField() {
+        TODO("Not yet implemented")
+    }
+
+    private fun persistCellData() {
+        TODO("Not yet implemented")
+    }
+
+    private fun translateBlock(point: Point, i: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun resetField() {
+        TODO("Not yet implemented")
+    }
+
+
+    enum class Statuses {
+        AWAITING_START, ACTIVE, INACTIVE, OVER
+    }
+
+    enum class Motions {
+        LEFT, RIGHT, DOWN, ROTATE
+    }
+}
